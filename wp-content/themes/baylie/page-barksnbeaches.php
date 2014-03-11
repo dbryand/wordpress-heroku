55b24f40fdd3137503e3879eeef3754d04bebd59
Dave Bryand <dave@davebryand.com>
2014-03-10 19:34:09 -0700

    Add barksnbeaches page.

---
+++ wp-content/themes/baylie/page-barksnbeaches.php
@@ -0,0 +1,186 @@
+<?php /*
+Template Name: PWYL: Barks-n-beaches
+*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Barks n Beaches</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    
+    <link rel="shortcut icon" href="img/favicon.png" />
+    <link href="/wp-content/themes/pwyl/barksnbeaches/css/bootstrap.css" rel="stylesheet">
+    <link href="/wp-content/themes/pwyl/barksnbeaches/css/styles.css" rel="stylesheet">
+    <link href="/wp-content/themes/pwyl/barksnbeaches/css/flexslider.css" rel="stylesheet">
+    <link href="/wp-content/themes/pwyl/barksnbeaches/css/jquery.fancybox-1.3.4.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900,300italic,400italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,200italic,300italic" rel="stylesheet" type="text/css">    
+    <link href="/wp-content/themes/pwyl/barksnbeaches/css/bootstrap-responsive.css" rel="stylesheet">                    
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-XXXXXXXX-XX', 'SITE-NAME');
+      ga('send', 'pageview');
+
+    </script>
+  </head>
+    
+  <body>
+      <section id="home" >
+          <div class="container">
+              <div class="row ">
+                  <div>
+                      <div id="branding">
+                        <a href="#about"></a>
+                        <p>Two things we love- our pups and the ocean (greater outdoors).</p> <p>Fight to protect what you love! #PWYL</p>
+                        <p><b>WTF- What the Flush?</b> If you’re like us, you probably haven’t given much thought to the 2 plastic poop bags per day that you’re personally scooping into landfill.</p>  <p>That’s about <b>730 bags</b> for each of us.  In a city like San Francisco with <b>120k dogs</b> that’s over <b>87 MILLION</b> plastic poop bags per year.</p>
+                        <p>Help us to increase awareness. Participate in the 1000 flush challenge.</p>
+                        <a href="#" id="call-to-action">Take the #GiveaShit Pledge</a>
+                      </div>
+                      <div id="arrow" class="hidden-phone"><a href="#about"></a></div>
+                      <div id="home_bg" class="hidden-phone hidden-tablet" data-stellar-background-ratio="0.5" data-stellar-vertical-offset="0"></div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      
+      <div class="nav-container hidden-phone">
+          <nav>
+              <div id="logo_small"><a href="#home"><img src="/wp-content/themes/pwyl/barksnbeaches/img/logo_small.png" alt="Logo small" ></a></div>
+              <ul>
+                  <li><a href="#home" class="selected">Home</a></li>
+                  <li><a href="#about">Why?</a></li>
+                  <li><a href="#services">Get Involved</a></li>
+                  <li><a href="#start_project">Buy Bags</a></li>
+              </ul>
+          </nav>
+      </div>
+      
+      <section id="about">
+          <div class="content">
+              <div class="container">
+                  <div class="row">
+                      <div class="span12">
+                          <h2 class="centered">Why should I care?</h2>
+                          <div class="row margin90">
+                              <blockquote class="span12">Besides giving our furry loved ones a great place to romp and swim, the ocean provides 50% of our oxygen (literally the air we breathe), 97% of the world’s water, protein for over a billion people, medical advancements, and $500 billion in global economy.</blockquote>
+                              
+                              <div class="span12">
+                              
+                              <hr>
+                                <h4 class="centered">Bad news</h4>
+                                  <p>The ocean is at risk due to plastics, pollution, climate change, over-fishing, etc.  The Pacific Ocean is the world’s largest landfill with an estimated 3-5million tons of trash.  We may not be throwing our poop bags directly into the ocean, but we are polluting the ocean and environment with our waste.</p>
+                                  
+                                  <hr>
+                                  
+                                  <h4 class="centered">Good news</h4>
+                                  <p>There are things we can do to help…use flushable dog poop bags, carry re-usable water bottles, bringing bags to the market, carpooling or taking public transportation, eating local foods.  Learn more at <a target="_blank" href="https://www.xprove.com/pwyl/the-ocean-and-you/">The Ocean and You</a>.</p>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </section>
+      
+      <section id="services" >
+          <div class="content">
+              <div class="container">
+                  <div class="row">
+                      <div class="span12 centered">
+                          <h2>How can I participate? <br>1000 flush #GiveaShitPledge</h2>
+                      </div>
+                      
+                      <div class="span12 margin90 services_list">
+                          <div class="row">
+                              <div class="span12 services_item">
+                                  <h4>It's simple!</h4>
+                                  <p>Our goal is to come together as a community and prevent 1000 poop bags from entering landfill (pledge by March 23rd - National Puppy Day).  That’s at least 20 dog families (2 per day #14scoops) and a couple dog walkers/daycare (10 per day, #140scoops) for 2 weeks.</p>
+                              </div>
+                          </div>
+                          
+                          <hr>
+                          
+                          <div class="row">
+                              <div class="span12 services_item">
+                                  <h4>Your role:</h4>
+                                  <ol>
+                                    <li>Pledge by March 23rd to use flushable bags for 2 weeks by tweeting a photo of your dog with #GiveaShitPledge #pwyl @barksnbeaches; state your pledge!</li>
+                                    <li>Follow us @barksnbeaches so we can keep you posted on progress & exchange ideas.</li>
+                                    <li>Recruit at least 2 other pledge via social media, phone, email post this on facebook, your blog, etc</li>
+                                  </ol>
+                              </div>
+                          </div>
+                          
+                          <hr>
+                          
+                          <div class="row">
+                              <div class="span9 services_item">
+                                  <h4>Goal:  1000 flushes pledged by March 23rd - National Puppy Day <br> (flushes completed by April 28th - Earth Day)</h4>
+                                  <h4>Extra incentives: Stay tuned!</h4>
+                              </div>
+
+                              <div class="span3">
+                                <ul class="list-social">
+                                  <li class="facebook"><a target="_blank" href="https://www.facebook.com"></a></li>
+                                  <li class="twitter"><a target="_blank" href="https://twitter.com/barksnbeaches"></a></li>
+                                </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div> 
+               </div>
+          </div>
+      </section>
+      
+      <section id="start_project">
+          <div class="container" >
+              <div class="row">
+                  <div class="span12 centered">
+                      <h2>Where do I get the flushable poop bags?</h2>
+                      <h1><a target="_blank" href="http://www.amazon.com/Flush-Puppies-Flushable-Certified-Compostable/dp/B004K51K3C/ref=pd_bxgy_petsupplies_img_z">Order your flushable poop bags here!</a></h1>
+                      
+                      <p>We’ll donate any referral fees back to SPCA and WAVES foundation.</p>
+                  </div>
+                  
+              </div>
+          </div>
+      </section>
+      
+      <section id="footer">
+          <div class="container" >
+              <div class="row">
+                  <div class="span12 centered">
+                      <p>Copyrights © Barks n Beaches - 2014.  San Francisco, CA.  All Rights Reserved.</p>
+                      
+                  </div>
+                  
+              </div>
+          </div>
+      </section>
+  </body>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/bootstrap.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/jquery.scrollTo.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/waypoints.min.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/jquery.stellar.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/jquery.flexslider.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/jquery.fancybox-1.3.4.pack.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/jquery.gmap.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/navbar.js"></script>
+    <script src="/wp-content/themes/pwyl/barksnbeaches/js/custom.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+</html>
