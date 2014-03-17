027921fc9d63fc3703e4030c577f84d0fdca075f
Dave Bryand <dave@davebryand.com>
2014-03-17 12:55:14 -0700

    Add GA to Barksnbeaches.

--- wp-content/themes/baylie/page-barksnbeaches.php
+++ wp-content/themes/baylie/page-barksnbeaches.php
@@ -24,17 +24,17 @@ Template Name: PWYL: Barks-n-beaches
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-XXXXXXXX-XX', 'SITE-NAME');
-      ga('send', 'pageview');
+  ga('create', 'UA-47962206-5', 'xprove.com');
+  ga('send', 'pageview');
 
-    </script>
-  </head>
+</script>
+</head>
     
   <body>
       <section id="home" >
